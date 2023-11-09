2c4e7da8f3ebdb3cf44b062cf849c5819f338aa4
gilad <mictavim@gmail.com>
2023-11-09 11:09:23 +0200

    new starter 9-10

--- src/config/index.js
+++ src/config/index.js
@@ -1,46 +1,30 @@
-const env = process.env.REACT_APP_ENV || "prod";
+const env = process.env.ENV || "dev";
 export const AvailableLanguages = {
   he: "he",
   en: "en",
 };
 const LOCAL_SERVER = "localhost";
-const DEV_SERVER = "localhost";
-const staging_SERVER = "localhost";
-// const PROD_SERVER = "localhost";
-const PROD_SERVER = "3.79.151.83";
+// const PROD_SERVER = "prod";
 
-const local = {
+const dev = {
   apiBaseHost: `http://${LOCAL_SERVER}:5000/api/gb`,
 };
-const dev = {
-  apiBaseHost: `https://${DEV_SERVER}:5000/api/gb`,
-};
-const staging = {
-  apiBaseHost: `https://${staging_SERVER}:5000/api/gb`,
-};
 const prod = {
-  apiBaseHost: `https://${PROD_SERVER}:5000/api/gb`,
+  apiBaseHost: `api/gb`,
 };
 
 const envConfigs = {
-  local,
   dev,
-  staging,
   prod,
 };
-/**
- * Generate config according to env
- */
 
 export const apiEndPoint = () => {
   return envConfigs[env];
 };
 
 const ClientConfig = {
-  mapsApiKey: "AIzaSyBFuDLMINZot5gFc0brTb_toEAk-bSVqVQ", // if you want to change api key, you need to change this also in index.html
   defaultLanguage: AvailableLanguages.en,
 
-  // Get all environment configurations
   ...envConfigs[env],
 };
 
--- src/services/Auth.js
+++ src/services/Auth.js
@@ -2,16 +2,16 @@ import { apiEndPoint } from "../config/index";
 const API_ENDPOINT = apiEndPoint().apiBaseHost;
 
 export const Login = async (data) => {
-  console.log("API_ENDPOINT", API_ENDPOINT);
-  console.log("data at login", data);
+  // console.log("API_ENDPOINT", API_ENDPOINT);
   let EndPoint = API_ENDPOINT + "/login";
   let res = await fetch(EndPoint, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      referrerPolicy: "unsafe_url",
     },
     body: JSON.stringify(data),
   });
-console.log("res",res)
+  console.log("res", res);
   return res;
 };

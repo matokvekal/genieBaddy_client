7d15946e5dd17e5e750699cd2582a9e77305441f
gilad <mictavim@gmail.com>
2023-12-13 15:56:27 +0200

    fix ui

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,34 +1,64 @@
 import moment from 'moment';
 
 export const formatDate = (date) => {
-  const currentDate = moment(date);
-  const now = moment();
+    const currentDate = moment(date);
+    const now = moment();
 
-  // Calculate differences in various units
-  const diffMinutes = now.diff(currentDate, 'minutes');
-  const diffHours = now.diff(currentDate, 'hours');
-  const diffDays = now.diff(currentDate, 'days');
-  const diffWeeks = now.diff(currentDate, 'weeks');
-  const diffMonths = now.diff(currentDate, 'months');
-  const diffYears = now.diff(currentDate, 'years');
+    // Calculate differences in various units
+    const diffMinutes = now.diff(currentDate, 'minutes');
+    const diffHours = now.diff(currentDate, 'hours');
+    const diffDays = now.diff(currentDate, 'days');
+    const diffWeeks = now.diff(currentDate, 'weeks');
 
-  if (diffMinutes < 5) {
-    return "Now";
-  } else if (diffMinutes < 60) {
-    return `${diffMinutes} minute${diffMinutes > 1 ? 's' : ''}`;
-  } else if (diffHours < 24) {
-    return `${diffHours} hour${diffHours > 1 ? 's' : ''}`;
-  } else if (diffDays === 1) {
-    return "Yesterday";
-  } else if (diffDays < 7) {
-    return `${diffDays} day${diffDays > 1 ? 's' : ''}`;
-  } else if (currentDate.isSame(now, 'week')) {
-    return "This week";
-  } else if (currentDate.isSame(now, 'month')) {
-    return "This month";
-  } else if (diffMonths < 12) {
-    return `${diffMonths} month${diffMonths > 1 ? 's' : ''}`;
-  } else {
-    return `${diffYears} year${diffYears > 1 ? 's' : ''}`;
-  }
+    if (diffMinutes < 10) {
+        return "now";
+    } else if (diffMinutes < 60) {
+        return `${diffMinutes} min`;
+    } else if (now.isSame(currentDate, 'day')) {
+        return currentDate.format('HH:mm');
+    } else if (diffDays < 7) {
+        return `${diffDays} days`;
+    } else if (now.isSame(currentDate, 'year')) {
+        return currentDate.format('DD-MMM');
+    } else {
+        return currentDate.format('DD-MM-YY');
+    }
 };
+
+
+//////////////////////old/////////////////////////
+
+// import moment from 'moment';
+
+// export const formatDate = (date) => {
+//   const currentDate = moment(date);
+//   const now = moment();
+
+//   // Calculate differences in various units
+//   const diffMinutes = now.diff(currentDate, 'minutes');
+//   const diffHours = now.diff(currentDate, 'hours');
+//   const diffDays = now.diff(currentDate, 'days');
+//   const diffWeeks = now.diff(currentDate, 'weeks');
+//   const diffMonths = now.diff(currentDate, 'months');
+//   const diffYears = now.diff(currentDate, 'years');
+
+//   if (diffMinutes < 5) {
+//     return "Now";
+//   } else if (diffMinutes < 60) {
+//     return `${diffMinutes} minute${diffMinutes > 1 ? 's' : ''}`;
+//   } else if (diffHours < 24) {
+//     return `${diffHours} hour${diffHours > 1 ? 's' : ''}`;
+//   } else if (diffDays === 1) {
+//     return "Yesterday";
+//   } else if (diffDays < 7) {
+//     return `${diffDays} day${diffDays > 1 ? 's' : ''}`;
+//   } else if (currentDate.isSame(now, 'week')) {
+//     return "This week";
+//   } else if (currentDate.isSame(now, 'month')) {
+//     return "This month";
+//   } else if (diffMonths < 12) {
+//     return `${diffMonths} month${diffMonths > 1 ? 's' : ''}`;
+//   } else {
+//     return `${diffYears} year${diffYears > 1 ? 's' : ''}`;
+//   }
+// };
